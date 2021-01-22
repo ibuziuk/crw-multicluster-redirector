8204a3e46a5c54c202b92258bbce87e05c86bf91
Ilya Buziuk <ibuziuk@redhat.com>
2021-01-22 15:38:16 +0100

    Adding external js
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/main/resources/META-INF/resources/redirector.js
@@ -0,0 +1,6 @@
+const queryString = window.location.search;
+console.log("Query String ", queryString);
+
+// setTimeout(function () {
+//     window.location.href = 'https://developers.redhat.com/developer-sandbox';
+// }, 5000);
