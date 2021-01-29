5fa5f811d802d661d2eeca36870a1e9a3cc67a40
Ilya Buziuk <ibuziuk@redhat.com>
2021-01-29 12:31:17 +0100

    Adding RootFilter for redirection to the '/'
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/main/java/io/developer/sandbox/crw/multicluster/redirector/filter/RootFilter.java
@@ -0,0 +1,41 @@
+package io.developer.sandbox.crw.multicluster.redirector.filter;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = "/*")
+public class RootFilter extends HttpFilter {
+
+    private static final long serialVersionUID = 1L;
+    private static final Pattern FILE_NAME_PATTERN = Pattern.compile(".*[.][a-zA-Z\\d]+");
+
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        chain.doFilter(request, response);
+
+        if (response.getStatus() == 404) {
+            String path = request.getRequestURI().substring(
+                    request.getContextPath().length()).replaceAll("[/]+$", "");
+            if (!FILE_NAME_PATTERN.matcher(path).matches()) {
+                // We could not find the resource, i.e. it is not anything known to the server (i.e. it is not a REST
+                // endpoint or a servlet), and does not look like a file so redirecting to the root
+                try {
+                    response.sendRedirect("/");
+                } finally {
+                    response.getOutputStream().close();
+                }
+            }
+        }
+    }
+
+}
